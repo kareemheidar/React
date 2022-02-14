9d4a16bcc917d7d3fe6426d455eb23c325bb6216
Kareem Heidar <k.abukhater@gmail.com>
2022-02-14 02:44:11 +0200

    Uncontrolled Forms

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 class Header extends Component {
 
@@ -28,18 +28,18 @@ class Header extends Component {
                         <NavbarBrand className="mr-auto" href="/"><img src="assets/images/LOGO22.png" height="40" width="40" alt="Ristorante Con Fusion" /></NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
-                            <NavItem>
-                                <NavLink className="nav-link"  to='/home'>&emsp;<span className="fa fa-home"></span> Home</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to='/aboutus'><span className="fa fa-info"></span> About Us</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link"  to='/menu'><span className="fa fa-list"></span> Menu</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card"></span> Contact Us</NavLink>
-                            </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to='/home'>&emsp;<span className="fa fa-home"></span> Home</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to='/aboutus'><span className="fa fa-info"></span> About Us</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to='/menu'><span className="fa fa-list"></span> Menu</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card"></span> Contact Us</NavLink>
+                                </NavItem>
                             </Nav>
                         </Collapse>
                     </div>
@@ -50,6 +50,11 @@ class Header extends Component {
                             <div className="col-12 col-sm-6">
                                 <h1>Ristorante con Fusion</h1>
                                 <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
+                            </div>
+                            <div className="col-12 col-sm-3 offset-sm-3">
+                                <Link to="/home">
+                                    <img src="assets/images/LOGO2.png" alt="Ristorante Con Fusion" />
+                                </Link>
                             </div>
                         </div>
                     </div>
